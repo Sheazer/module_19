3c83fa00cdf4f003f6dd49061f59b30f6a386496
module_18_2 <erjan03iopro@gmail.com>
2024-11-15 16:26:23 +0600

    fourth commit. admin panel

--- task1/admin.py
+++ task1/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
-# Register your models here.
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name','age', 'balance', )
+    search_fields = ('name','balance')
+    list_filter = ('balance','name',)
+    list_per_page = 10
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description',)
+    search_fields = ('title', 'description', )
