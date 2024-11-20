cdf4353c750cf9bed687c0880caf34e09e4250b0
module_18_2 <erjan03iopro@gmail.com>
2024-11-21 00:23:21 +0600

    6th commit. new app the posts

--- posts/views.py
+++ posts/views.py
@@ -8,7 +8,18 @@ def post_list(request):
 
     posts = Post.objects.all()
 
-    items_per_page = request.GET.get('items', 5)
+    # Проверяем наличие параметра 'items' в GET-запросе
+    items_per_page = request.GET.get('items')
+
+    if items_per_page:
+        # Сохраняем значение в сессии, если оно есть
+        request.session['items_per_page'] = items_per_page
+    else:
+        # Используем сохранённое в сессии значение, если оно есть
+        items_per_page = request.session.get('items_per_page')
+
+    # Преобразуем в число, если значение найдено, иначе применяем безопасное значение по умолчанию
+    items_per_page = int(items_per_page) if items_per_page else 5
 
     paginator = Paginator(posts, items_per_page)
 
